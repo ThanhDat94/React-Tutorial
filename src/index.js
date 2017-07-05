fe9b14eb15c4a58d93987297ebcbdb38232f4305
Thanh Dat <tdat.nguyen@gmail.com>
2017-07-05 17:24:43 +0700

    Day 2

--- src/index.js
+++ src/index.js
@@ -3,54 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-function formateDate(date){
-  return date.toLocaleDateString();
-}
-function Comment(props){
-  return(
-    <div className="Comment">
-         <UserInfo userId={props.author}/>
-         <div className="Comment-text">
-            {props.text}
-          </div>
-          <div className="Comment-date">
-            {formateDate(props.date)}
-          </div>
-    
-    </div>
 
-  )
-}
-function Avatar(props){
-  return(
-    <img className="Avatar" 
-                  src={props.user.avatarUrl}
-                  alt={props.user.name} 
-              />
-  )
-}
-function UserInfo(props){
-    return(    <div className="UserInfo">
-                <Avatar user={props.userId} />
-              <div className="UserInfo-name">
-                {props.name}
-              </div>
-         </div>
+class Clock extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={date:new Date()}
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+
+    return (
+       <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+
     )
-}
-const comment={
-  date:new Date(),
-  text:'I hope you enjoy learning React !',
-  author:{
-    name:'Hello Kitty',
-    avatarUrl:'http://placekitten.com/g/64/64'
   }
 }
-ReactDOM.render( 
-  <Comment date={comment.date}
-           text={comment.text}
-           author={comment.author}   />,
+  ReactDOM.render( 
+  <Clock/>,
    document.getElementById('root'));
+
+
+   
 
 
 registerServiceWorker();
